905b2813e47f81ff10033ccf2725f6135bcc4b0b
Ethan Madden <crazeh.monkeh@gmail.com>
2015-12-23 15:04:46 -0800

    Basic repl-style loop

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+  "bufio"
+  "fmt"
+  "os"
+)
+
 
 func main() {
-  fmt.Printf("hello world\n")
+  fmt.Printf("You are standing in an open cube inside of a white office full of sound-dampening walls. There are new emails in your inbox.\n")
+  reader := bufio.NewReader(os.Stdin)
+  for {
+    fmt.Printf("> ")
+    input, err := reader.ReadString('\n')
+    if(err != nil){
+      fmt.Println(err)
+    }
+    fmt.Println(input)
+  }
 }
